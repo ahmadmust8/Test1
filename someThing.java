fb0549ce4e81fdc5da3de745818e969a620cb70d
Ahmad Mostafa <ahmadmust8@gmail.com>
2017-06-04 08:58:55 +0300

    update somethng.java

--- someThing.java
+++ someThing.java
@@ -1,9 +1,17 @@
-class Demo extands EveryThing {
-Demo(){
+puplic final class you extands EveryThing {
+
+double lifeTime = EveryThing.yourLifeTime;
+
+ArrayList<String> yourProblems = new ArrayList();
+
+yourProblems.add("i don't know ?");
+yourProblems.add("realy!!! i don't know ); ");
+
+boolean areYouMerred = false;
+double yourAge = 24.6;
+
+puplic you(){
 super(this);
-}
 
-
-
-
+    }
 }
